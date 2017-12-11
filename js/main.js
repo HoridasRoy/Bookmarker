91f66a072fc2f1ed417977a11143c95993a11fc8
Horidas Roy <horidasr@gmail.com>
2017-12-12 00:29:58 +0600

    some work of js is done

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+document.getElementById('myform').addEventListener('submit',saveBookmark);
+
+function saveBookmark(e)
+{
+	console.log('It works ..')
+
+	var SiteName = document.getElementById('SiteName').value;
+	var SiteUrl = document.getElementById('SiteUrl').value;
+
+	var Bookmark ={
+
+		name: SiteName,
+		url : SiteUrl
+	}
+
+	/*
+	localStorage.setItem('test','Hello world');
+	console.log(localStorage.getItem('test'));
+	localStorage.removeItem('test');
+	console.log(localStorage.getItem('test'));
+
+	*/
+	if(localStorage.getItem('bookmarks') == null)
+	{
+		var bookmarks = [];
+
+		bookmarks.push(Bookmark);
+
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	}
+
+	else{
+
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+		bookmarks.push(Bookmark);
+
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+	}
+	e.preventDefault();
+}
